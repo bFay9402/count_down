4ea77acb7ab15a4fe25370c58f04b6927fbfc50e
bFay9402 <bfay9402@gmail.com>
2021-06-15 15:09:48 -0400

    quick count down timer

---
+++ app.js
@@ -0,0 +1,18 @@
+let countDownDate = new Date("Oct 8, 2021 00:00:00");
+
+let counter = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 *24));
+  let hours = Math.floor(distance % (1000 * 60 * 60 *24) / (1000 * 60 * 60));
+  let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+  document.querySelector('#timer').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+  if(distance < 0) {
+    clearInterval(counter);
+    document.querySelector('#timer').innerHTML = 'THE WAIT IS OVER!!!!';
+  }
+
+}, 1000);
